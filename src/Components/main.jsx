eac0a3734bfe44f889f42ab82036a9458e7cd8bf
Shubham Jha <jhashubham976@gmail.com>
2023-05-23 21:34:26 +0530

    Added success page

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -2,11 +2,14 @@ import React,{useState} from 'react'
 import {Button,Modal} from 'react-bootstrap'
 import '../Style/main.css'
 import Group from '../assets/Group.png'
+import Vector from '../assets/Vector.png'
+import Fox from '../assets/Fox.png'
 function Main() {
     const [modal,setModal]=useState(false)
     const [modal1,setModal1]=useState(false)
     const [modal2,setModal2]=useState(false)
     const [modal3,setModal3]=useState(false)
+    const [modal4,setModal4]=useState(false)
   return (
     <div className="main" style={{padding:"2%",margin:"0 12% 0 15%"}}>
       <div className="mainFirstRow" style={{width:"100%",}}><div style={{paddingTop:"10px"}}><svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" style={{fill:"#12D576"}} viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></div>
@@ -70,7 +73,7 @@ function Main() {
         <input placeholder="Enter recipient address"type="text" style={{width:"100%",height:"50px",borderRadius:"5px",border:"1px solid #949494"}}/>
         <div style={{width:"100%",paddingTop:"15%",display:"flex",flexDirection:"row",jsutifyContent:"space-between",gap:"52%"}}>
         <Button onClick={()=>{setModal(false) }} style={{backgroundColor:"transparent",color:"#12D576",border:"2px solid #12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Cancel</Button>
-      <Button onClick={()=>{setModal(false) }} style={{backgroundColor:"#12D576",border:"#12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Submit</Button>
+      <Button onClick={()=>{setModal4(true);setModal(false) }} style={{backgroundColor:"#12D576",border:"#12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Submit</Button>
         </div>
       </Modal.Body>
     </Modal>
@@ -91,7 +94,7 @@ function Main() {
         <input placeholder="e.g 50000"type="text" style={{width:"100%",height:"50px",borderRadius:"5px",border:"1px solid #949494"}}/>
         <div style={{width:"100%",paddingTop:"15%",display:"flex",flexDirection:"row",jsutifyContent:"space-between",gap:"52%"}}>
         <Button onClick={()=>{setModal1(false) }} style={{backgroundColor:"transparent",color:"#12D576",border:"2px solid #12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Cancel</Button>
-      <Button onClick={()=>{setModal1(false) }} style={{backgroundColor:"#12D576",border:"#12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Submit</Button>
+      <Button onClick={()=>{setModal4(true);setModal1(false) }} style={{backgroundColor:"#12D576",border:"#12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Submit</Button>
         </div>
       </Modal.Body>
     </Modal>
@@ -112,7 +115,7 @@ function Main() {
         <input placeholder="Enter owner address"type="text" style={{width:"100%",height:"50px",borderRadius:"5px",border:"1px solid #949494"}}/>
         <div style={{width:"100%",paddingTop:"15%",display:"flex",flexDirection:"row",jsutifyContent:"space-between",gap:"52%"}}>
         <Button onClick={()=>{setModal2(false) }} style={{backgroundColor:"transparent",color:"#12D576",border:"2px solid #12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Cancel</Button>
-      <Button onClick={()=>{setModal2(false) }} style={{backgroundColor:"#12D576",border:"#12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Submit</Button>
+      <Button onClick={()=>{setModal4(true);setModal2(false) }} style={{backgroundColor:"#12D576",border:"#12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Submit</Button>
         </div>
       </Modal.Body>
     </Modal>
@@ -133,8 +136,49 @@ function Main() {
 <div style={{padding:"10% 12%"}}>If you renounce ownership , you will lose the ability to manage the token.</div>
 <div style={{width:"100%",paddingTop:"15%",display:"flex",flexDirection:"row",jsutifyContent:"space-between",gap:"52%"}}>
         <Button onClick={()=>{setModal3(false) }} style={{backgroundColor:"transparent",color:"#12D576",border:"2px solid #12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Cancel</Button>
-      <Button onClick={()=>{setModal3(false) }} style={{backgroundColor:"#12D576",border:"#12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Confirm</Button>
+      <Button onClick={()=>{setModal4(true);setModal3(false) }} style={{backgroundColor:"#12D576",border:"#12D576",padding:"7px 25px",fontSize:"20px",fontWeight:"450"}} variant="">Confirm</Button>
         </div>
+      </Modal.Body>
+    </Modal>
+    <Modal
+      {...{show:modal4,
+        onHide:() => setModal4(false)}}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+
+    >
+        <Modal.Header closeButton>
+      </Modal.Header>
+      <Modal.Body>
+      <div style={{paddingTop:"8%",textAlign:"center",paddingLeft:"9%",paddingRight:"9%"}}>
+<img src={Vector} alt="not found" />
+<h5 style={{fontWeight:"700",paddingTop:"10px"}}>Token created successfully, view it on the block explorer</h5>
+</div>
+<div style={{padding:"3% 8%",display:"flex",flexDirection:"row",justifyContent:"space-between"}}>
+    <div>
+        <div >Token Address</div>
+        <div style={{paddingTop:"30%"}}>Token Link</div>
+    </div>
+    <div>
+        <div style={{color:"#2D5C8F"}}>
+<svg width="17" height="20" viewBox="0 0 17 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2 20C1.45 20 0.979002 19.804 0.587002 19.412C0.195002 19.02 -0.000664969 18.5493 1.69779e-06 18V5C1.69779e-06 4.71667 0.0960018 4.479 0.288002 4.287C0.480002 4.095 0.717335 3.99934 1 4C1.28333 4 1.521 4.096 1.713 4.288C1.905 4.48 2.00067 4.71734 2 5V18H12C12.2833 18 12.521 18.096 12.713 18.288C12.905 18.48 13.0007 18.7173 13 19C13 19.2833 12.904 19.521 12.712 19.713C12.52 19.905 12.2827 20.0007 12 20H2ZM6 16C5.45 16 4.979 15.804 4.587 15.412C4.195 15.02 3.99934 14.5493 4 14V2C4 1.45 4.196 0.979002 4.588 0.587002C4.98 0.195002 5.45067 -0.000664969 6 1.69779e-06H15C15.55 1.69779e-06 16.021 0.196002 16.413 0.588002C16.805 0.980002 17.0007 1.45067 17 2V14C17 14.55 16.804 15.021 16.412 15.413C16.02 15.805 15.5493 16.0007 15 16H6ZM6 14H15V2H6V14Z" fill="#2882E3"/>
+</svg>1519815165068165165162+965+6</div>
+        <div style={{color:"#2D5C8F",paddingTop:"10%"}}>
+<svg width="17" height="20" viewBox="0 0 17 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2 20C1.45 20 0.979002 19.804 0.587002 19.412C0.195002 19.02 -0.000664969 18.5493 1.69779e-06 18V5C1.69779e-06 4.71667 0.0960018 4.479 0.288002 4.287C0.480002 4.095 0.717335 3.99934 1 4C1.28333 4 1.521 4.096 1.713 4.288C1.905 4.48 2.00067 4.71734 2 5V18H12C12.2833 18 12.521 18.096 12.713 18.288C12.905 18.48 13.0007 18.7173 13 19C13 19.2833 12.904 19.521 12.712 19.713C12.52 19.905 12.2827 20.0007 12 20H2ZM6 16C5.45 16 4.979 15.804 4.587 15.412C4.195 15.02 3.99934 14.5493 4 14V2C4 1.45 4.196 0.979002 4.588 0.587002C4.98 0.195002 5.45067 -0.000664969 6 1.69779e-06H15C15.55 1.69779e-06 16.021 0.196002 16.413 0.588002C16.805 0.980002 17.0007 1.45067 17 2V14C17 14.55 16.804 15.021 16.412 15.413C16.02 15.805 15.5493 16.0007 15 16H6ZM6 14H15V2H6V14Z" fill="#2882E3"/>
+</svg>1519815165068165165162+965+6</div>
+    </div>
+</div>
+<div style={{textAlign:"center",paddingTop:"5%"}}>
+    <Button onClick={()=>{setModal3(false) }} style={{backgroundColor:"black",color:"white",padding:"7px 40px",fontSize:"20px",fontWeight:"450"}} variant="">
+        <img src={Fox} alt="" />
+        Add to Metamask</Button>
+        <br />
+      <Button onClick={()=>{setModal4(true);setModal3(false) }} style={{marginTop:"3%",backgroundColor:"#12D576",border:"#12D576",padding:"7px 40px",fontSize:"20px",fontWeight:"450"}} variant="">Go to Manage Token</Button>
+      <div style={{color:"#12D576",fontWeight:"600",fontSize:"17px",cursor:"pointer",paddingTop:"3%"}}>Go back to create token</div>
+      </div>
       </Modal.Body>
     </Modal>
       </div>
