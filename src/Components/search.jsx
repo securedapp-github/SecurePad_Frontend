7d252ccb3d7a2f437e5fe00e26735e1d207ae048
Shubham Jha <jhashubham976@gmail.com>
2023-06-09 23:55:48 +0530

    Search Page Improved

--- src/Components/search.jsx
+++ src/Components/search.jsx
@@ -4,7 +4,7 @@ import FACTORYABI from "../ABI/FactoryABI.json";
 import SALEABI from "../ABI/SaleABI.json";
 import TOKENABI from "../ABI/TokenABI.json";
 import {useNavigate} from 'react-router-dom'
-
+import Coin from '../assets/Coin.png';
 import { ethers } from "ethers";
 
 import {
@@ -360,41 +360,54 @@ const Card = ({ title, description, image, soft, hard, progress, liq, lock, end,
     }
     
     return (
-        <div onClick={() => {GoToSaleDetail(sale)}} style={{cursor:"pointer",backgroundColor:"rgba(70,70,70,0.4)",width:"25vw",borderRadius:"2vw"}}>
-        <div className="card">
+        <div onClick={() => {GoToSaleDetail(sale)}} style={{cursor:"pointer",borderRadius:"1.5vw"}}>
+        <div className="card" style={{borderRadius:"1.5vw",backgroundColor:"rgba(70,70,70,0.4)"}}>
             <img src={image} alt={title} className="card-image" />
+            <div style={{display:"flex",justifyContent:"space-between"}}>
+            <img src={Coin} style={{ paddingLeft: "2vw",position:"relative",bottom:"0.3vw",width:"5vw",height:"3vw" }} alt="not found" />
+            <div style={{display:"flex",paddingRight:"1.5vw",paddingTop:"1vw",gap:"0.1vw"}}>
+            <div style={{padding:"0.3vw 0.5vw",fontWeight:"700",color:'#12D576',border:"1px solid #12D576",borderRadius:"1vw"}}>Audit</div>
+            <div style={{padding:"0.3vw 0.5vw",fontWeight:"700",color:'#12D576',border:"1px solid #12D576",borderRadius:"1vw"}}>KYC+</div>
+            </div>
+            </div>
             <div className="card-content">
-                <h3 className="card-title">{title}</h3>
+                <h2 className="card-title">{title}</h2>
                 <p className="card-description">{description}</p>
                 <div className="progress-bar-container">
                     <span className="progress-text">Progress {progress}%</span>
-                    <progress className="progress-bar" value={progress} max="100"></progress>
+                    <progress className="progress-bar"  value={progress} max="100"></progress>
                     {/* <span className="value-text">5.073ETH </span>
         <span className="new-text">10BNB</span> */}
                 </div>
                 <div className="box-container">
-                    <div className="box box-1">
+                    <div className="box box-1" style={{fontSize:"0.6vw"}}>
                         <span className="box-text">Softcap</span>
                         <span className="Softcap-text">{soft} </span>
                     </div>
-                    <div className="box box-2">
+                    <div className="box box-2" style={{fontSize:"0.6vw"}}>
                         <span className="box-text">Hardcap</span>
                         <span className="Softcap-text">{hard} </span>
 
                     </div>
                     </div> <div className="box-container">
-                    <div className="box box-3">
+                    <div className="box box-3" style={{fontSize:"0.6vw"}}>
                         <span className="box-text">Liquidity</span>
                         <span className="Liquidity-text">{liq}%</span>
                     </div>
-                    <div className="box box-4">
+                    <div className="box box-4" style={{fontSize:"0.6vw"}}>
                         <span className="box-text">Locked</span>
                         <span className="Locked-text">{lock} Days  </span>
                     </div>
 
                 </div>
                 <div className="line"> </div>
-                <span className="end">{end}</span>
+                <div style={{display:"flex",justifyContent:"space-between"}}>
+                <div className="end" style={{fontSize:"0.8vw",width:"50%"}}>{end}</div>
+                <div style={{dispaly:"flex",position:"relative",bottom:"2.5vw"}}>
+                <svg height="2vw" width="2vw" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z"/></svg>
+                <svg height="2vw" width="2vw" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
+                </div>
+                </div>
                 <head>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
                 </head>
